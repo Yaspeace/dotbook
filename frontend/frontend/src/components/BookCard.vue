bda9fe367c6c001ed0aeaf8d012b17529595476f
Yaspeace <mikle0201@mail.ru>
2022-10-29 12:20:54 +0300

    карточька

---
+++ frontend/frontend/src/components/BookCard.vue
@@ -0,0 +1,34 @@
+<!--TODO: накинуть стили, разобраться с роутами-->
+<template>
+    <div class="book-card">
+          <div class="book-card-sub">
+            <img src=""/>
+            <div class="book-card-content">
+                <h3>{{name}}</h3>
+                <p>Автор(ы): {{authors}}</p>
+                <p>Год выпуска: {{year}}</p>
+                <p>Издание: {{publish}}</p>
+            </div>
+          </div>
+          <div class="book-card-footer">
+              <a class="btn btn-outline-dark" href="index.html">В избранное</a>
+              <a class="btn btn-dark" href="index.html"><b>Читать!</b></a>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+ name: 'BookCard',
+ props: {
+    name: String,
+    authors: String,
+    year: String,
+    publish: String
+ }   
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/frontend/src/views/HomeView.vue
+++ frontend/frontend/src/views/HomeView.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BookCard name="Паттерны ООП" 
+      authors="Э. Гамма, Р. Хелм, Р. Джонсон, Дж. Влиссидес"
+      year="2020"
+      publish="ООО Издательство Питер"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import BookCard from '@/components/BookCard.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    BookCard
   }
 }
 </script>
